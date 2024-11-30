a77fe0ac1a86b267f4b95130fcbddc2ef1576739
sxcben <90620852+sxcben@users.noreply.github.com>
2024-11-30 14:11:36 -0600

    update config

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 # Use you API key and secret
-api_key = 'your_api_key'
-api_secret = 'your_api_secret'
+api_key = 'PK5RZN11XAH8ZZFDUPNT'
+api_secret = 'L1AofM3BeGLGCRnRvUTUu8h1MVHSS16YOAoh6ixs'
 base_url = 'https://paper-api.alpaca.markets/v2'  # Use paper trading base URL for testing
+
+from alpaca.trading.client import TradingClient
+client = TradingClient(api_key, api_secret)
